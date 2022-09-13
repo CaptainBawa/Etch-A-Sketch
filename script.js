b357234d013f5c634a26c61e8c10c2d12691c0b1
Collins Bawa <collinskwamebawa@yahoo.com>
2022-09-13 18:02:54 +0000

    mouse over event listener added and now sketchpad is working

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-let board = document.querySelector('.board');
-board.style.gridTemplateColumns = `repeat(16, 1fr)`;
-board.styleTemplateRow = `repeat(16, 1fr)`;
+const createBoard = (size) => {
+    /* Selecting the board and the squares. */
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
 
-for(let i = 0; i < 256; i++){
+    /* Removing all the divs from the board. */
+    squares.forEach((div) => div.remove());
+
+   /* Setting the grid template columns and rows to the size of the board. */
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.styleTemplateRow = `repeat(${size}, 1fr)`;
+    
+    let amount = size * size;
+
+ /* This is creating a for loop that is creating a div element and adding an event listener to it. */
+ for(let i = 0; i < amount; i++){
     let square = document.createElement('div');
-    square.style.backgroundColor = 'blue';
+    square.addEventListener('mouseover', ()=> {
+        square.style.backgroundColor = 'black'
+    });
+
+    square.style.backgroundColor = 'white';
     board.insertAdjacentElement('beforeend', square);
-}
+ };
+ 
+};
+createBoard(16);
+
+
+const changeSize = (val) => {
+    if(val >= 2 && val <= 100){
+        createBoard(val);
+    }
+    else{
+        console.log('Error')
+    }
+    
+};
